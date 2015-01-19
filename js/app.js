028a807b20e818ace4159ad1a9f9613c109b92cf
Shin Uesugi <shinsaku417@gmail.com>
2015-01-19 12:32:54 -0800

    Now adds streams when inputing username

--- js/app.js
+++ js/app.js
@@ -10,10 +10,23 @@ angular.module('multistream', [])
 })
 .factory('Factory', function($http) {
   var searchStream = function(username) {
-    $http
+    $http.jsonp('https://api.twitch.tv/kraken/channels/' + username + '?callback=JSON_CALLBACK').success(function(data){
+      var iframe = document.createElement('iframe');
+      iframe.src = "http://www.twitch.tv/" + username + "/embed";
+      iframe.frameborder = "0";
+      iframe.scrolling = "no";
+      iframe.height = "378";
+      iframe.width = "400";
+      document.body.appendChild(iframe);
+    }).error(function (data) {
+      // something went wrong :(
+      console.log('Got nothing');
+    });
   };
 
   return {
     searchStream: searchStream
   };
 });
+
+// <iframe src="http://www.twitch.tv/faceittv/embed" frameborder="0" scrolling="no" height="378" width="620"></iframe>
