a5ffcb93ac61eed8674a4b5471dcf885ba046ab2
Shin Uesugi <shinsaku417@gmail.com>
2015-01-19 15:47:22 -0800

    Error message self removal

--- js/factory.js
+++ js/factory.js
@@ -4,22 +4,26 @@ angular.module('hexstream.factory', [])
   var streams = [];
 
   var searchTwitch = function(username) {
-    $http.jsonp('https://api.twitch.tv/kraken/channels/' + username + '?callback=JSON_CALLBACK').success(function(data){
-      if (data.error !== "Not Found") {
-        var iframe = document.createElement('iframe');
-        iframe.src = "http://www.twitch.tv/" + username + "/embed";
-        iframe.frameborder = "0";
-        iframe.scrolling = "no";
-        iframe.height = "378";
-        iframe.width = "425";
-        document.body.appendChild(iframe);
-        streams.push(iframe);
-      } else {
-        errorFeedback("Username " + username + " not found!");
-      }
-    }).error(function (data) {
-      errorFeedback("Oops! Something went wrong. Try again.");
-    });
+    if (streams.length < 6) {
+      $http.jsonp('https://api.twitch.tv/kraken/channels/' + username + '?callback=JSON_CALLBACK').success(function(data){
+        if (data.error !== "Not Found") {
+          var iframe = document.createElement('iframe');
+          iframe.src = "http://www.twitch.tv/" + username + "/embed";
+          iframe.frameborder = "0";
+          iframe.scrolling = "no";
+          iframe.height = "378";
+          iframe.width = "425";
+          document.body.appendChild(iframe);
+          streams.push(iframe);
+        } else {
+          errorFeedback("Username " + username + " not found!");
+        }
+      }).error(function (data) {
+        errorFeedback("Oops! Something went wrong. Try again.");
+      });
+    } else {
+      errorFeedback("Too many streams open!");
+    }
   };
 
   var errorFeedback = function(message) {
@@ -28,6 +32,9 @@ angular.module('hexstream.factory', [])
     errorMessage.style.color = "red";
     errorMessage.style.fontSize = "14px";
     angular.element(document.body.querySelector('.userInput')).append(errorMessage);
+    setTimeout(function() {
+      errorMessage.parentNode.removeChild(errorMessage);
+    }, 1500);
   };
 
   var removeStream = function(num) {
