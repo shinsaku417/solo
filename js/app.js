2ec6e6a4bbf93abe9ae8d3251c288123e2a184d0
Shin Uesugi <shinsaku417@gmail.com>
2015-01-19 13:30:57 -0800

    Filtering and clearing out submission button working

--- js/app.js
+++ js/app.js
@@ -1,31 +1,29 @@
 angular.module('multistream', [])
 
 .controller('multistreamCtrl', function ($scope, Factory) {
-  // Your code here
-  $scope.input = {};
-
-  $scope.searchStream = function() {
-    Factory.searchStream($scope.input.username);
+  $scope.searchTwitch = function() {
+    Factory.searchTwitch($scope.input.username);
+    $scope.input = {};
   }
 })
 .factory('Factory', function($http) {
-  var searchStream = function(username) {
+  var searchTwitch = function(username) {
     $http.jsonp('https://api.twitch.tv/kraken/channels/' + username + '?callback=JSON_CALLBACK').success(function(data){
-      var iframe = document.createElement('iframe');
-      iframe.src = "http://www.twitch.tv/" + username + "/embed";
-      iframe.frameborder = "0";
-      iframe.scrolling = "no";
-      iframe.height = "378";
-      iframe.width = "400";
-      document.body.appendChild(iframe);
+      if (data.error !== "Not Found") {
+        var iframe = document.createElement('iframe');
+        iframe.src = "http://www.twitch.tv/" + username + "/embed";
+        iframe.frameborder = "0";
+        iframe.scrolling = "no";
+        iframe.height = "378";
+        iframe.width = "450";
+        document.body.appendChild(iframe);
+      }
     }).error(function (data) {
-      // something went wrong :(
       console.log('Got nothing');
     });
   };
-
   return {
-    searchStream: searchStream
+    searchTwitch: searchTwitch
   };
 });
 
